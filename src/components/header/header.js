c703b4bdd90d50a706ca939079f890fbc31554a8
Vlad Tihomirov <tsikhamirau.uladzislau@student.ehu.lt>
2023-05-11 08:58:58 +0000

    Resolve "ESDU: Improve Header component"

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,82 @@
 import './header.css';
 import { getIconUrl } from '../../utils/utils';
 
-export const Header = () => {
-  const header = document.createElement('header');
+const CART_ICON_URL = getIconUrl('cart-icon.svg');
+const SEARCH_ICON_URL = getIconUrl('circle-arrow-icon-02.svg');
+const ACCOUNT_ICON_URL = getIconUrl('account-icon.svg');
+const TEMPLATE = `
+    <div class="icon-conteiner desktop-hidden" id="menu">
+      <div class="icon">
+        <div class="burger-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+      </div>
+      <div class="icon-title">MENU</div>
+    </div>
+    <div class="icon-container"></div>
 
-  header.classList.add('header');
-
-  const cartIconUrl = getIconUrl('cart-icon.svg');
-
-  header.innerHTML = `
     <a class="logo" href="#/">
         <span class="color--pink">Bloom</span> Beauty
     </a>
 
-    <img src="${cartIconUrl}" />
-    `;
+    <nav class="mobile-hidden">
+        <ul>
+            <li>SHOP ALL</li>
+            <li>BESTSELLERS</li>
+            <li>COLLECTION</li>
+            <li>ABOUT US</li>
+            <li>BLOG</li>
+        </ul>
+    </nav>
 
-  return header;
+    <div class="action-bar">
+      <div class="icon-container mobile-hidden">
+        <div class="icon">
+            <img src="${SEARCH_ICON_URL}" alt="Search Icon Image"/>
+        </div>
+        <div class="icon-title">SEARCH</div>
+      </div>
+      <div class="icon-container mobile-hidden">
+        <div class="icon">
+            <img src="${ACCOUNT_ICON_URL}" alt="Account Icon Image"/>
+        </div>
+        <div class="icon-title">ACCOUNT</div>
+      </div>
+      <div class="icon-container">
+        <div class="icon">
+            <img src="${CART_ICON_URL}" alt="Cart Icon Image"/>
+        </div>
+        <div class="icon-title">CART</div>
+      </div>
+    </div>
+    <div class="menu-list">
+        <div class="menu-list_header">
+            <div class="menu-list_header-text">MENU</div>
+        </div>
+        <ul class="menu-list_body">
+          <li><a href="#/catalog">CATALOG</a></li>
+          <li><a href="#/blog">BLOG</a></li>
+          <li>SHOP ALL </li>
+          <li>BESTSELLERS</li>
+          <li>COLLECTION</li>
+          <li>ABOUT US</li>
+        </ul>
+    </div>
+  `;
+
+export function onMenuBurgerClicked() {
+  const headerElement = document.getElementsByTagName('header')[0];
+  headerElement.classList.toggle('opened');
+}
+
+export const Header = () => {
+
+  const headerElement = document.createElement('header');
+
+  headerElement.classList.add('header');
+  headerElement.innerHTML = TEMPLATE;
+
+  return headerElement;
 };
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './style.css';
 import router from './plugins/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import {Controller} from "./utils/controller";
 
 setTimeout(() => {
   document.querySelector('#app').prepend(Header());
   document.querySelector('#app').append(Footer());
+  Controller();
 });
