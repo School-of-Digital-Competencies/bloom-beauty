ac5389aee16606f3e095e459c20529fbf0c39a88
Grasevich Yuliya <juliesg@tut.by>
2023-05-11 08:53:37 +0000

    Resolve "ESDU: Add Share your Blossomed Section to HomePage"

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,9 @@
 import { Hero } from '../components/hero/hero';
+import { Share } from '../components/share/share';
 
 export const HomePage = () => {
   return `
     ${Hero()}
+    ${Share()}
   `;
 };
