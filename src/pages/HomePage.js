5913959184141f000331041d9d011a93dcfa707a
Anastasia Romanova <anastasiya.erohina@gmail.com>
2023-05-11 08:57:55 +0000

    Resolve "ESDU: Add The Skin Quiz component to HomePage"

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import { Hero } from '../components/hero/hero';
+import { Quiz } from '../components/quiz/quiz';
 import { Share } from '../components/share/share';
 
 export const HomePage = () => {
   return `
     ${Hero()}
+    ${Quiz()}
     ${Share()}
   `;
 };
