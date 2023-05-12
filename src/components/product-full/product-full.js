b24e76ed02f0b2ba1af3d9342f8e7fc5f4a0d8b3
vladtihomirov <vladtihomirov010203@gmail.com>
2023-05-12 12:19:11 +0300

    feat(ESDU-00): Created dynamic reviews

--- src/components/product-full/product-full.js
+++ src/components/product-full/product-full.js
@@ -1,13 +1,52 @@
 import './product-full.css';
 import { getProductImageUrl } from '../../utils/utils';
+import {reviewArray} from "../../api/reviews";
+
+function getReviewHTMLs(reviews, productId) {
+  let result = '';
+  reviews.filter(x => x.productId === productId).forEach((review, i) => {
+    const {id, imgUrl, rating, username, isVerified, title, description, attachments} = review;
+    let ratingHTML = '';
+    for (let j = 0; j < rating; j++) {
+      ratingHTML += '<div class="star"><img src="src/assets/icons/star.png" alt=""></div>';
+    }
+    for (let j = 0; j < 5-rating; j++) {
+      ratingHTML += '<div class="star"><img src="src/assets/icons/star-alt.png" alt=""></div>';
+    }
+    result += `
+      <div class="review" id="${id}">
+        <div class="review__header_logo"><img src="${imgUrl}" alt=""></div>
+          <div class="review__content">
+            <div class="review__header">
+              <div class="review__header_desc">
+                <div class="review__header__username">${username}</div>
+                <div class="stars">
+                  ${ratingHTML}
+                </div>
+              </div>
+            </div>
+            <div class="review__body">
+              <div class="review__title">${title}</div>
+              <div class="review__desc">${description}</div>
+            </div>
+          </div>
+        </div>
+      <div class="review__divider"></div>
+    `;
+  });
+  return result;
+}
+
 
 export const ProductFull = (product) => {
   const { id, title, description, img, price } = product;
   const imgUrl = getProductImageUrl(img);
   const smallImgUrl = imgUrl.slice(0,-4);
+
+  const reviews = getReviewHTMLs(reviewArray, id);
   return `
     <article class="product-full">
-    <div class=product-images> 
+    <div class=product-images>
 
         <img class="product-full__image" src="${imgUrl}" />
         <div class="product-small_images">
@@ -35,5 +74,25 @@ export const ProductFull = (product) => {
 
   </div>
 </article>
+
+<div class="review__section">
+  <h3 id="review-title">Read the reviews</h3>
+  <button class="small">
+     see all
+  </button>
+  <div class="rating-container" id="top-rating">
+    <div class="stars">
+      <div class="star"><img src="src/assets/icons/star.png" alt=""></div>
+      <div class="star"><img src="src/assets/icons/star.png" alt=""></div>
+      <div class="star"><img src="src/assets/icons/star.png" alt=""></div>
+      <div class="star"><img src="src/assets/icons/star.png" alt=""></div>
+      <div class="star"><img src="src/assets/icons/star.png" alt=""></div>
+    </div>
+    <div class="count__reviews">4 review</div>
+  </div>
+  <div class="reviews">
+  ${reviews}
+  </div>
+</div>
     `;
 };
