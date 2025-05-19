8b73ee4a4d7df4d8f19c87c405c3d8bc844b08e9
vittoriabortn <bortnikova.viktoria@student.ehu.lt>
2025-05-19 17:08:28 +0300

    feat: fix banner according to design

--- src/components/hero/hero.ts
+++ src/components/hero/hero.ts
@@ -4,7 +4,8 @@ export const Hero = () => {
   return `
     <section class="hero">
       <h1 class="hero__title">
-        Discover your inner beauty with Blossom Glow Kit
+        Discover your inner beauty <br />
+        with Blossom Glow Kit
       </h1>
 
       <p class="hero__description">Great gift for yourself and loved ones</p>
